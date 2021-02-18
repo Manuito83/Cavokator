b7d1fa6cd2a39bd2d50e9ad068457513472f4a90
Manuel Ortega <manuortega@gmail.com>
2021-02-18 23:21:30 +0100

    Reseted API key

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GMSServices provideAPIKey:@"AIzaSyC2r0K_S7hTT1jQw2PQNxPpSnXMKy88hDA"];
+  [GMSServices provideAPIKey:@"#### KEY RESETED ####"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
